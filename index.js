311cd51fd17b98a5a028c4f5425db6b03316ce16
Steve Manuel <steve@dylib.so>
2024-01-19 10:20:39 -0700

    chore: another test

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ async function actism(input, steps, wasi, outputType, test) {
   steps = steps.trim();
   outputType = outputType.trim();
 
-  let githubToken = core.getInput('github_token');
+  const githubToken = core.getInput('github_token');
   if (githubToken.length === 0) {
     core.info("No `github_token` was read from input");
   }
